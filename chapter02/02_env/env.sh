ac9e8119735367f0d19c2c5688857b2c0e0fbe16
devchloe <devchloe.kwon@gmail.com>
2022-02-12 15:08:14 +0900

    correct yaml and sh

--- chapter02/02_env/env.sh
+++ chapter02/02_env/env.sh
@@ -3,21 +3,23 @@ kubectl apply -f hello-app.yaml
 
 # Pod 실행 및 IP 확인
 kubectl get pod -o wide
+kubectl get pod/hello-app -o json
 
-# 컨테이너 IP 확인 (ifconfig eth0)
+# 컨테이너 IP 확인
 kubectl exec hello-app -- ifconfig eth0
+kubectl exec hello-app -- cat /etc/hosts
 
 # 컨테이너에 설정된 환경변수 확인 (env)
 kubectl exec hello-app -- env
 
-# 컨테이너가 리스닝하고 있는 포트 확인 (netstat -a)
-kubectl exec hello-app -- netstat -a
+# 컨테이너가 리스닝하고 있는 포트 확인
+kubectl exec hello-app -- netstat -an
 
-# 로컬 포트 포워딩  (3000 -> 3000)
-kubectl port-forward hello-app 3000:3000
+# 로컬 포트 포워딩  (8080 -> 8080)
+kubectl port-forward hello-app 8080:8080
 
 # HTTP Server 응답 확인  (웹브라우저 or curl)
-curl localhost:3000
+curl -v localhost:8080
 
 # Pod 종료
 kubectl delete pod --all
