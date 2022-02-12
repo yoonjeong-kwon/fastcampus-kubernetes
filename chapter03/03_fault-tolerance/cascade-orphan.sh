50a43adb4af156f0166ee05b9ab21c4be18a8cb4
devchloe <devchloe.kwon@gmail.com>
2022-02-12 21:02:44 +0900

    update sh and yaml

--- chapter03/03_fault-tolerance/cascade-orphan.sh
+++ chapter03/03_fault-tolerance/cascade-orphan.sh
@@ -11,15 +11,15 @@ kubectl apply -f replicaset.yaml
 # Pod의 Owner 정보 확인
 kubectl get pod <pod-name> -o jsonpath="{.metadata.ownerReferences[0].name}"
 
-# ReplicaSet만 삭제
+# ReplicaSet만 삭제 (orphan: 고아 전략)
 kubectl delete rs blue-replicaset --cascade=orphan
-
-# Pod의 Owner 정보 확인
-kubectl get pod <pod-name> -o jsonpath="{.metadata.ownerReferences[0].name}"
 
 # ReplicaSet과 Pod 목록 확인
 kubectl get rs
 kubectl get pod
+
+# Pod의 Owner 정보 확인
+kubectl get pod <pod-name> -o jsonpath="{.metadata.ownerReferences[0].name}"
 
 # ReplicaSet의 replicas를 2로 변경 후 재배포
 kubectl apply -f replicaset.yaml
