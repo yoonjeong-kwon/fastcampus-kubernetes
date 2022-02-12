e730eb9439f28660a540762bad935d5f56ea3212
devchloe <devchloe.kwon@gmail.com>
2022-02-12 15:35:28 +0900

    update sh and yaml

--- chapter02/03_networking/networking.sh
+++ chapter02/03_networking/networking.sh
@@ -3,7 +3,7 @@ kubectl apply -f blue-green-app.yaml
 kubectl apply -f red-app.yaml
 
 # Pod 실행 및 IP 확인
-kubectl get pod –o wide
+kubectl get pod -o wide
 
 # 컨테이너 IP 확인 (ifconfig eth0)
 kubectl exec blue-green-app -c blue-app -- ifconfig eth0
@@ -18,27 +18,27 @@ kubectl logs red-app -c red-app
 # 컨테이너에 설정된 환경변수 확인 (printenv)
 kubectl exec blue-green-app -c blue-app -- printenv POD_IP NAMESPACE NODE_NAME
 kubectl exec blue-green-app -c green-app -- printenv POD_IP NAMESPACE NODE_NAME
-kubectl exec blue-green-app -c red-app -- printenv POD_IP NAMESPACE NODE_NAME
+kubectl exec red-app -c red-app -- printenv POD_IP NAMESPACE NODE_NAME
 
 # blue-app 컨테이너 -> green-app 컨테이너 /tree, /hello 요청 실행
-kubectl exec blue-green-app -c blue-app -- curl -s localhost:8081/tree
-kubectl exec blue-green-app -c blue-app -- curl -s localhost:8081/hello
+kubectl exec blue-green-app -c blue-app -- curl -vs localhost:8081/tree
+kubectl exec blue-green-app -c blue-app -- curl -vs localhost:8081/hello
 
 # green-app 컨테이너 -> blue-app 컨테이너 /sky, /hello 요청 실행
-kubectl exec blue-green-app -c green-app -- curl -s localhost:8080/sky
-kubectl exec blue-green-app -c green-app -- curl -s localhost:8080/hello
+kubectl exec blue-green-app -c green-app -- curl -vs localhost:8080/sky
+kubectl exec blue-green-app -c green-app -- curl -vs localhost:8080/hello
 
 # blue-app 컨테이너 -> red-app 컨테이너 /rose, /hello 요청 실행
 export RED_POD_IP=$(kubectl get pod red-app -o jsonpath="{.status.podIP}")
 echo $RED_POD_IP
-kubectl exec blue-green-app -c blue-app -- curl -s $RED_POD_IP:8080/rose
-kubectl exec blue-green-app -c blue-app -- curl -s $RED_POD_IP:8080/hello
+kubectl exec blue-green-app -c blue-app -- curl -vs $RED_POD_IP:8080/rose
+kubectl exec blue-green-app -c blue-app -- curl -vs $RED_POD_IP:8080/hello
 
 # red-app 컨테이너 -> blue-app 컨테이너 /sky, /hello 요청 실행
 export BLUE_POD_IP=$(kubectl get pod blue-green-app -o jsonpath="{.status.podIP}")
 echo $BLUE_POD_IP
-kubectl exec red-app -- curl -s $BLUE_POD_IP:8080/sky
-kubectl exec red-app -- curl -s $BLUE_POD_IP:8080/hello
+kubectl exec red-app -- curl -vs $BLUE_POD_IP:8080/sky
+kubectl exec red-app -- curl -vs $BLUE_POD_IP:8080/hello
 
 # 포트포워딩을 통해 웹브라우저로 각 컨테이너 요청/응답 확인
 kubectl port-forward blue-green-app 8080:8080
