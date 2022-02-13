69a597a2f117007849377efdf6e336da62d7a0ba
Yoonjeong <yoonjeong@Yoonjeongs-iMac.local>
2022-02-14 00:32:10 +0900

    update sh

--- chapter03/02_replication/replication-2.sh
+++ chapter03/02_replication/replication-2.sh
@@ -1,11 +1,15 @@
-# ReplicaSet 목록 조회 (Watch 모드)
-kubectl get rs -w
-
-# Pod 목록 조회 (Watch 모드)
-kubectl get pod -w 
-
 # app=blue-app 레이블을 가진 단독 팟 생성
 kubectl apply -f blue-app.yaml
+
+# replicaset.yaml을 배포
+kubectl apply -f replicaset.yaml
+
+
+# ReplicaSet 목록 조회 (Watch 모드로 켜두고 ReplicaSet의 변화를 관찰할 수 있다)
+kubectl get rs -w
+
+# Pod 목록 조회 (Watch 모드로 켜두고 ReplicaSet의 변화를 관찰할 수 있다)
+kubectl get pod -w 
 
 # ReplicaSet의 Pod 생성 기록 확인 (kubectl describe)
 kubectl describe rs blue-replicaset
