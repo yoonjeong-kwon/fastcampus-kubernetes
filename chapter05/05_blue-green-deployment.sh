bdea16d8c38fa7228f9cd4821a03c990f29e2048
devchloe <devchloe.kwon@gmail.com>
2022-02-20 23:05:58 +0900

    update blue-green

--- chapter05/05_blue-green-deployment.sh
+++ chapter05/05_blue-green-deployment.sh
@@ -6,9 +6,7 @@ kubectl apply -f deployment/05_deployment-order.yaml
 kubectl apply -f service/05_service-blue-green.yaml
 
 # 3. 주문 홈 - order.fast-snackbar.com 응답 확인
-kubectl port-forward service/order-app 8080:80 -n order
-
-curl -sv order.fast-snackbar.com
+curl -sv -H "Host: order.fast-snackbar.com" --request GET $INGRESS_IP
 
 # 4. 1.0 버전 정리
 # kubectl get rs -n order
